5df19c7d0a02c2e5f4fd5b911e030e00a821a699
maeldcls <maelduclos03@gmail.com>
2023-07-10 14:51:44 +0200

    finished randomanswer

--- process/randomAnswer.php
+++ process/randomAnswer.php
@@ -1,9 +1,22 @@
 <?php
-    $statement = $db->prepare("SELECT * FROM questions WHERE id = 1");
+require_once('connexion.php');
+
+    $statement = $db->prepare("SELECT answer1,answer2,answer3,goodanswer FROM questions WHERE idquestion = 1");
     $statement->execute();
     $question = $statement->fetch();
 
-    do{
-        
-    }while($bool)
+    $reponses= [0=>$question[0], 1=>$question[1],2=>$question[2], 3=>$question[3]];
+    $reponsesRand = [];
+
+    for($i=0; $i<=3;$i++){
+        $bool = true;
+        do{
+            $rand = rand(0,3);
+            if(empty($reponsesRand[$rand])){
+                $reponsesRand[$rand] = $reponses[$i];
+                $bool = false;
+            }
+        }while($bool);
+    }
+    var_dump($reponsesRand);
 ?>
