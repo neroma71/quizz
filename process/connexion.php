7b0cd9a48d1d6fdc4a968758ff17346ec1569b54
neroma71 <neroma71@gmail.com>
2023-07-10 16:34:48 +0200

    test reponse

--- process/connexion.php
+++ process/connexion.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 $dns = 'mysql:host=localhost;dbname=quizz';
 $user = 'root';
 $password = '';
@@ -12,17 +11,4 @@ try{
 catch (Exception $message){
     echo "ya un problème de soucis<br>" . "<pre>$message</pre>" ;
 }
-=======
-    $dns = 'mysql:host=localhost;dbname=quizz' ;
-    $user = 'root';
-    $password = '';
-
-    try{
-        $db = new PDO($dns, $user, $password);
-    }
-    catch(Exception $message){
-        echo "impossible de se connectre" . "<pre>$message</pre>";
-    }
-
->>>>>>> quizz
 ?>
